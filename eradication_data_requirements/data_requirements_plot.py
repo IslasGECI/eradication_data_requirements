72f869dfcd0962ba96acc71cb6eb0afaffe38822
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-01 10:31:25 -0700

    ✅ Pass test

--- eradication_data_requirements/data_requirements_plot.py
+++ eradication_data_requirements/data_requirements_plot.py
@@ -1,2 +1,2 @@
-def data_requirements_plot():
+def data_requirements_plot(input_path, output_path):
     pass
--- tests/test_data_requirements_plot.py
+++ tests/test_data_requirements_plot.py
@@ -2,5 +2,7 @@ import eradication_data_requirements as dt
 
 
 def test_data_requirements_plot():
-    dt.data_requirements_plot()
+    input_path = "/workdir/tests/data/cumulative_effort_and_captures_for_year.csv"
+    output_path = "/workdir/tests/data/yearly_ramsy_plot.png"
+    dt.data_requirements_plot(input_path, output_path)
     pass

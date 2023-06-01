d53373e592d72dd04c7f15dfd444628967ca7229
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-01 18:37:40 +0000

    ✅ Pass tests

--- eradication_data_requirements/data_requirements_plot.py
+++ eradication_data_requirements/data_requirements_plot.py
@@ -1,2 +1,19 @@
+import pandas as pd
+import numpy as np
+from geci_plots import plt, geci_plot
+
+
+def fit_ramsey_plot(data):
+    return np.polyfit(data["Captures"], data["CPUE"], 1)
+
+
 def data_requirements_plot(input_path, output_path):
-    pass
+    data = pd.read_csv(input_path)
+    theta = fit_ramsey_plot(data.drop([0]))
+    y_line = theta[1] + theta[0] * data["Captures"]
+    fig, ax = geci_plot()
+    plt.scatter(data["Captures"], data["CPUE"], marker="o")
+    plt.plot(data["Captures"], y_line, "r")
+    plt.xlabel("Cumulative captures", size=15, labelpad=15)
+    plt.ylabel("CPUE (captures/night traps)", size=15)
+    plt.savefig(output_path, dpi=300, transparent=True)

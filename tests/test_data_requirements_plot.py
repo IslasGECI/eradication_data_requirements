332a50d30ca6d761e9e2a5d6447cdb19e3bdbd57
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-01 17:50:31 +0000

    ♻️  Refactor

--- tests/test_data_requirements_plot.py
+++ tests/test_data_requirements_plot.py
@@ -1,8 +1,11 @@
+import os
 import eradication_data_requirements as dt
 
 
 def test_data_requirements_plot():
     input_path = "/workdir/tests/data/cumulative_effort_and_captures_for_year.csv"
     output_path = "/workdir/tests/data/yearly_ramsy_plot.png"
+    if os.path.exists(output_path):
+        os.remove(output_path)
     dt.data_requirements_plot(input_path, output_path)
     pass

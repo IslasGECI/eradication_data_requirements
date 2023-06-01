b47658bab89838df037805c9734540a740a5ee4e
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-06-01 10:26:09 -0700

    😇✅🔥 Delete dummy test

--- tests/test_transformations.py
+++
@@ -1,9 +0,0 @@
-import eradication_data_requirements as dt
-
-
-def test_add_offset():
-    augend = 1
-    addend = 2
-    expected = augend + addend
-    obtained = dt.add_offset(augend, addend)
-    assert expected == obtained

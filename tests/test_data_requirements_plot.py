7e48622e0c482e5806f8f805cc066592101a371f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-01 18:36:56 +0000

    🛑🧪 Fail tests

--- tests/test_data_requirements_plot.py
+++ tests/test_data_requirements_plot.py
@@ -5,11 +5,11 @@ import eradication_data_requirements as dt
 
 def test_data_requirements_plot():
     input_path = "/workdir/tests/data/cumulative_effort_and_captures_for_year.csv"
-    output_path = "/workdir/tests/data/yearly_ramsy_plot.png"
+    output_path = "/workdir/tests/data/yearly_ramsey_plot.png"
     if os.path.exists(output_path):
         os.remove(output_path)
     dt.data_requirements_plot(input_path, output_path)
     file_content = open(output_path, "rb").read()
     obtained_hash = hashlib.md5(file_content).hexdigest()
-    expected_hash = "145db61b30a8246e9563378d1db5cf7e"
+    expected_hash = "3ac61e336449f0f96c83cb2b11b86101"
     assert obtained_hash == expected_hash

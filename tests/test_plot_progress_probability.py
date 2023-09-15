dd2b4b21c9f15c025cdfde381ab8ce7f72fed8b3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-15 21:09:58 +0000

    ♻️  Refactor

--- tests/test_plot_progress_probability.py
+++ tests/test_plot_progress_probability.py
@@ -7,4 +7,15 @@ import matplotlib as plt
 def test_plot_progress_probability():
     data = pd.read_csv("tests/data/progress_probability_tests.csv")
     obtained = plot_progress_probability(data)
+    plt.pyplot.savefig("figure.png")
     assert isinstance(obtained, plt.axes._axes.Axes)
+
+    obtained_y_label = obtained.get_ylabel()
+    expected_y_label = "Progress probability"
+    assert obtained_y_label == expected_y_label
+
+    obtained_ylim = plt.pyplot.ylim()
+    expected_ylim = (-0.05, 1.05)
+    assert obtained_ylim == expected_ylim
+    assert obtained.get_xticklabels()[0].get_text() == "2015-07"
+    # assert obtained.get_xticklabels()[0].get_position()[0] == 6

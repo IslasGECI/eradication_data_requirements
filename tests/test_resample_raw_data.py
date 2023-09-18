33810c562abf5cfe420f6e3062692f79b79832d8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-18 20:36:17 +0000

    🛑🧪 Fail tests

--- tests/test_resample_raw_data.py
+++ tests/test_resample_raw_data.py
@@ -9,7 +9,7 @@ def test_resample_valid_data():
     seed = 42
     obtained = resample_valid_data(effort_and_capture_data[0:5], seed)
     expected_len = 6
-    obtained_len = len(obtained)
+    obtained_len = obtained[0].shape[0]
     assert obtained_len == expected_len
 
     seed = 3

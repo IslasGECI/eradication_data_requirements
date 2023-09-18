b8a07020174a7e426d74e5b1f0a4752389a3a36f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-18 18:47:13 +0000

    🛑🧪 Fail tests

--- tests/test_resample_raw_data.py
+++ tests/test_resample_raw_data.py
@@ -1,10 +1,13 @@
 from eradication_data_requirements import resample_valid_data
 
+import pandas as pd
+
 effort_and_capture_data = pd.read_csv("tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv")
 
 
 def test_resample_valid_data():
     obtained = resample_valid_data(effort_and_capture_data[:, 0:5])
+    print(obtained)
     expected_len = 6
     obtained_len = obtained.shape[0]
     assert obtained_len == expected_len

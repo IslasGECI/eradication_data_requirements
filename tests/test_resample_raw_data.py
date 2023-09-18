5029fbeaaeb061dcc357dd48d8c2b9defa114952
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-18 20:07:14 +0000

    🛑🧪 Fail tests

--- tests/test_resample_raw_data.py
+++ tests/test_resample_raw_data.py
@@ -1,4 +1,4 @@
-from eradication_data_requirements import resample_valid_data
+from eradication_data_requirements import resample_valid_data, validate_samples_to_fit
 
 import pandas as pd
 
@@ -10,4 +10,14 @@ def test_resample_valid_data():
     obtained = resample_valid_data(effort_and_capture_data[0:5], seed)
     expected_len = 6
     obtained_len = obtained.shape[0]
+    assert obtained_len == expected_len
+
+
+def test_validate_samples_to_fit():
+    non_valid_data = pd.DataFrame({"Capturas": [14, 0, 0, 0, 0, 0]})
+    valid_data = pd.DataFrame({"Capturas": [14, 1, 0, 8, 0, 3]})
+    sampled_data = [non_valid_data, valid_data]
+    obtained = validate_samples_to_fit(sampled_data)
+    expected_len = 1
+    obtained_len = len(obtained)
     assert obtained_len == expected_len

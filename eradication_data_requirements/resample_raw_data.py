e7b5a459f35613682eb37805c0ee3097498b6001
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-18 20:15:54 +0000

    ✅ Pass tests

--- eradication_data_requirements/resample_raw_data.py
+++ eradication_data_requirements/resample_raw_data.py
@@ -3,3 +3,13 @@ from bootstrapping_tools import resample_data
 
 def resample_valid_data(effort_and_capture_data, seed):
     return resample_data(effort_and_capture_data, seed, blocks_length=2)
+
+
+def validate_samples_to_fit(samples):
+    validated = [
+        valid_sample
+        for valid_sample in samples
+        if len(valid_sample.Capturas.unique()) > 1
+        and valid_sample.Capturas.sum() != valid_sample.Capturas[0]
+    ]
+    return validated

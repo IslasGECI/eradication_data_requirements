0e37a8554aaf14a3a5c0807d59c5c05d75bb1ce8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-20 18:32:13 +0000

    🛑🧪 Fail tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,3 +8,8 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def tests_api_write_effort_and_captures_with_probability():
+    response = client.get("/write_effort_and_captures_with_probability")
+    assert response.status_code == 200

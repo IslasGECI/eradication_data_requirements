c788b1147c3637faef9f88bda9d829c9c128008a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-20 18:33:23 +0000

    ✅ Pass tests

--- eradication_data_requirements/api.py
+++ eradication_data_requirements/api.py
@@ -6,3 +6,8 @@ api = FastAPI()
 @api.get("/")
 async def read_main():
     return {"msg": "Hello World"}
+
+
+@api.get("/write_effort_and_captures_with_probability")
+async def api_write_effort_and_captures_with_probability():
+    pass

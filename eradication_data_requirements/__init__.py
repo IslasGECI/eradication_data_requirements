88737479d827299fb5abe4ef29c7329415023abb
Mario Villasante <mario.villasante@islas.org.mx>
2023-09-20 09:52:36 -0700

    🔥⚰️ Remove dead code

--- eradication_data_requirements/__init__.py
+++ eradication_data_requirements/__init__.py
@@ -5,5 +5,4 @@ from .cli import *  # noqa
 from .data_requirements_plot import *  # noqa
 from .fit_ramsey_time_series import *  # noqa
 from .plot_progress_probability import *  # noqa
-from .remove_consecutive_non_captures import *  # noqa
 from .resample_raw_data import *  # noqa

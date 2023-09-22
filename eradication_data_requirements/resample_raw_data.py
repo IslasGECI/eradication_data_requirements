77c887520895e52948435dde49b060f3bc8b2ae0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-09-22 16:49:29 +0000

    ♻️  Refactor

--- eradication_data_requirements/resample_raw_data.py
+++ eradication_data_requirements/resample_raw_data.py
@@ -2,15 +2,22 @@ from bootstrapping_tools import resample_data
 
 
 def resample_valid_data(effort_and_capture_data, bootstrapping_number):
-    blocks_length = 2
-    sample = generate_samples(effort_and_capture_data, bootstrapping_number, blocks_length)
-    return validate_samples_to_fit(sample)
+    validate_method = validate_samples_to_fit
+    return resample_valid_data_by_method(
+        effort_and_capture_data, bootstrapping_number, validate_method
+    )
 
 
 def resample_valid_cumulative_data(cumulative_captures_data, bootstrapping_number):
     blocks_length = 2
     samples = generate_samples(cumulative_captures_data, bootstrapping_number, blocks_length)
     return validate_cumulative_samples_to_fit(samples)
+
+
+def resample_valid_data_by_method(effort_and_capture_data, bootstrapping_number, validate_method):
+    blocks_length = 2
+    sample = generate_samples(effort_and_capture_data, bootstrapping_number, blocks_length)
+    return validate_method(sample)
 
 
 def generate_samples(cumulative_captures_data, bootstrapping_number, blocks_length):

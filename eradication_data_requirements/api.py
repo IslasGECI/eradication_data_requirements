0d4a47d3c04270552c02338c6e0000d8f410faa7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-10-10 21:11:02 +0000

    ✅ Pass tests

--- eradication_data_requirements/api.py
+++ eradication_data_requirements/api.py
@@ -2,6 +2,7 @@ from eradication_data_requirements.cli import (
     write_effort_and_captures_with_probability,
     write_progress_probability_figure,
 )
+from eradication_data_requirements.data_requirements_plot import data_requirements_plot
 from fastapi import FastAPI
 
 api = FastAPI()
@@ -20,3 +21,8 @@ async def api_write_effort_and_captures_with_probability(
 @api.get("/write_probability_figure")
 async def api_write_probability_figure(input_path: str, output_path: str):
     write_progress_probability_figure(input_path, output_path)
+
+
+@api.get("/plot_cpue_vs_cum_captures")
+async def api_plot_cpue_vs_cum_captures(input_path: str, output_path: str):
+    data_requirements_plot(input_path, output_path)

d8f6e89ba65cee653969b58f6c938c61275582d1
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 18:50:41 +0000

    ♻️  Refactor

--- tests/test_plot_cpue_series.py
+++ tests/test_plot_cpue_series.py
@@ -9,13 +9,11 @@ import pandas as pd
 import matplotlib as mpl
 
 
-cpue_data_path = "tests/data/processed_yearly_cpue_for_plot.csv"
-output_png = "tests/data/annual_cpue_time_series.png"
-
-
 def test_plot_cumulative_series_cpue():
+    cpue_data_path = "tests/data/processed_yearly_cpue_for_plot.csv"
     cpue_df = pd.read_csv(cpue_data_path)
     fontsize = 20
+    output_png = "tests/data/annual_cpue_time_series_2.png"
     obtained = plot_cumulative_series_cpue(output_png, fontsize, cpue_df)
     assert isinstance(obtained[0], mpl.axes._axes.Axes)
 
@@ -24,6 +22,7 @@ def tests_plot_cumulative_series_cpue_by_season():
     effort_capture_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
     effort_capture_df = pd.read_csv(effort_capture_path)
     font_size = 27
+    output_png = "tests/data/annual_cpue_time_series.png"
     gtt.if_exist_remove(output_png)
     plot_cumulative_series_cpue_by_season(effort_capture_df, output_png, font_size)
     expected_hash = "1026fc4e26d645bbeba7fdae0245e52e"

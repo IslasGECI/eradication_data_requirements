95369e1a6d53625387f36920c62875d4ede7dc3a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 17:52:58 +0000

    🛑🧪 Fail tests

--- tests/test_plot_cpue_series.py
+++ tests/test_plot_cpue_series.py
@@ -7,7 +7,7 @@ import pandas as pd
 def tests_plot_cumulative_series_cpue():
     effort_capture_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
     effort_capture_df = pd.read_csv(effort_capture_path)
-    output_png = "tests/figures/annual_cpue_time_series.png"
+    output_png = "tests/data/annual_cpue_time_series.png"
     font_size = 27
     plot_cumulative_series_cpue(effort_capture_df, output_png, font_size)
     expected_hash = "1026fc4e26d645bbeba7fdae0245e52e"

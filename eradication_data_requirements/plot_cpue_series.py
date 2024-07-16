9aae2fb42e9e6d2621dc4bf477d463f5c1754a67
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 20:14:05 +0000

    ♻️  Refactor

--- eradication_data_requirements/plot_cpue_series.py
+++ eradication_data_requirements/plot_cpue_series.py
@@ -9,32 +9,32 @@ def plot_cumulative_series_cpue_by_season(effort_capture_df, output_png, fontsiz
     plt.savefig(output_png, dpi=300, transparent=True)
 
 
-def plot_cumulative_series_cpue(fontsize, data_year):
-    seasons = data_year.index.values
+def plot_cumulative_series_cpue(fontsize, cpue_df):
+    seasons = cpue_df.index.values
     seasons_labels = [*seasons, ""]
     ticks_positions = np.arange(seasons[0], seasons[-1] + 2)
     ticks_positions[-1] = ticks_positions[-1] + 0.25
 
     _, ax = plt.subplots(1, 2, figsize=(23, 10), tight_layout=True)
 
-    ax[0].plot(seasons, data_year["cpue"], "-o", linewidth=2)
+    ax[0].plot(seasons, cpue_df["cpue"], "-o", linewidth=2)
     ax[0].set_xticks(ticks_positions)
     ax[0].set_xticklabels(seasons_labels, size=fontsize)
     ax[0].tick_params(axis="both", labelsize=fontsize)
     ax[0].spines["right"].set_visible(False)
-    max_cpue = max(data_year["cpue"])
+    max_cpue = max(cpue_df["cpue"])
     cpue_limit = roundup(max_cpue, 10 ** order_magnitude(max_cpue))
     ax[0].set_ylim(0, cpue_limit)
     ax[0].set_ylabel("Catch Per Unit Effort (CPUE)", fontsize=fontsize)
     ax[0].set_xlim(ticks_positions[0] - 1, ticks_positions[-1])
 
-    ax[1].plot(seasons, data_year["cumulative_cpue"], "-o", linewidth=2)
+    ax[1].plot(seasons, cpue_df["cumulative_cpue"], "-o", linewidth=2)
     ax[1].set_xticks(ticks_positions)
     ax[1].set_xticklabels(seasons_labels, size=fontsize)
     ax[1].tick_params(axis="both", labelsize=fontsize)
     ax[1].spines["right"].set_visible(False)
     ax[1].spines["top"].set_visible(False)
-    max_cum_cpue = max(data_year["cumulative_cpue"])
+    max_cum_cpue = max(cpue_df["cumulative_cpue"])
     cum_cpue_limit = roundup(max_cum_cpue, 10 ** order_magnitude(max_cum_cpue))
     ax[1].set_ylim(0, cum_cpue_limit)
     ax[1].set_ylabel("Cumulative CPUE", fontsize=fontsize)

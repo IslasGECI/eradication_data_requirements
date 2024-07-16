6d2797f23e32502bfceda3ea6729def22f72e61d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 17:49:47 +0000

    🛑🧪 Fail tests

--- tests/test_plot_cpue_series.py
+++ tests/test_plot_cpue_series.py
@@ -1,3 +1,4 @@
+from eradication_data_requirements.plot_cpue_series import plot_cumulative_series_cpue
 import geci_test_tools as gtt
 
 import pandas as pd
@@ -5,10 +6,10 @@ import pandas as pd
 
 def tests_plot_cumulative_series_cpue():
     effort_capture_path = "tests/data/esfuerzo_capturas_mensuales_gatos_socorro.csv"
-    effort_capture_df = pd.read_csv(effort_captures_path)
+    effort_capture_df = pd.read_csv(effort_capture_path)
     output_png = "tests/figures/annual_cpue_time_series.png"
     font_size = 27
-    plot_cumulative_series_cpue(effort_capture, output_png, font_size)
-    expected_hash = "7b05eb42a7500976d2e13f3c490dbc66"
+    plot_cumulative_series_cpue(effort_capture_df, output_png, font_size)
+    expected_hash = "1026fc4e26d645bbeba7fdae0245e52e"
     obtained_hash = gtt.calculate_hash(output_png)
     assert obtained_hash == expected_hash

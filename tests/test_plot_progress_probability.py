d9511a635ad85f3c26f4d5c90a9e7b4cbe4c5da7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-16 18:41:48 +0000

    🛑🧪 Fail tests

--- tests/test_plot_progress_probability.py
+++ tests/test_plot_progress_probability.py
@@ -9,8 +9,7 @@ import matplotlib as plt
 def test_plot_progress_probability():
     data = pd.read_csv("tests/data/progress_probability_tests.csv")
     obtained = plot_progress_probability(data)
-    print(obtained)
-    assert isinstance(obtained[0], plt.axes._axes.Axes)
+    assert isinstance(obtained, plt.axes._axes.Axes)
 
     obtained_y_label = obtained.get_ylabel()
     expected_y_label = "Progress probability"

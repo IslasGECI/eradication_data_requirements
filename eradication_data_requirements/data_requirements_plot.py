0ecef8fb79f37a48f62cbccedb006c51d26ec50a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-07-25 20:34:09 +0000

    ♻️  Refactor

--- eradication_data_requirements/data_requirements_plot.py
+++ eradication_data_requirements/data_requirements_plot.py
@@ -18,8 +18,8 @@ def plot_comparative_catch_curves(socorro_path, guadalupe_path, output_path):
     socorro_data = pd.read_csv(socorro_path)
     guadalupe_data = pd.read_csv(guadalupe_path)
     _, ax = geci_plot()
-    ax = xxplot_catch_curve(socorro_data, ax, "Socorro")
-    ax = xxplot_catch_curve(guadalupe_data, ax, "Guadalupe")
+    ax = plot_catch_curve(socorro_data, ax, "Socorro")
+    ax = plot_catch_curve(guadalupe_data, ax, "Guadalupe")
     plt.xlabel("Cumulative captures", size=15, labelpad=15)
     plt.ylabel("CPUE (captures/night traps)", size=15)
     plt.legend(fontsize="xx-large")
@@ -37,7 +37,7 @@ def XXdata_requirements_plot(input_path, output_path, species="cat"):
     config_plot = SPECIES_CONFIG
     configured_data = rename_goat_date(data)
     _, ax = geci_plot()
-    ax = xxplot_catch_curve(configured_data, ax)
+    ax = plot_catch_curve(configured_data, ax)
     plt.xlabel("Cumulative captures", size=15, labelpad=15)
     plt.ylabel(config_plot[species]["ylabel"], size=15)
     plt.savefig(output_path, dpi=300, transparent=True)
@@ -58,16 +58,9 @@ def rename_goat_date(data):
     return data.rename(columns=SPECIES_CONFIG["goat"])
 
 
-def xxplot_catch_curve(data, ax, label=None):
+def plot_catch_curve(data, ax, label=None):
     theta = fit_ramsey_plot(data.drop([0]))
     y_line = theta[1] + theta[0] * data["Cumulative_captures"]
     ax.plot(data["Cumulative_captures"], y_line, "r")
     ax.scatter(data["Cumulative_captures"], data["CPUE"], marker="o", label=label)
     return ax
-
-
-def plot_catch_curve(data, label=None):
-    theta = fit_ramsey_plot(data.drop([0]))
-    y_line = theta[1] + theta[0] * data["Cumulative_captures"]
-    plt.plot(data["Cumulative_captures"], y_line, "r")
-    plt.scatter(data["Cumulative_captures"], data["CPUE"], marker="o", label=label)

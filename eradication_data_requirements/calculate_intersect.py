9b684838b718907931366fe20d3958c6680f6233
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-12 20:38:57 +0000

    ✅ Pass tests

--- eradication_data_requirements/calculate_intersect.py
+++ eradication_data_requirements/calculate_intersect.py
@@ -1,6 +1,12 @@
 from eradication_data_requirements import fit_ramsey_plot
 
 
+def get_intercepts_distribution(raw_data, bootstrap_number):
+    return [
+        calculate_x_intercept(resample_eradication_data(raw_data)) for _ in range(bootstrap_number)
+    ]
+
+
 def resample_eradication_data(data):
     resampled_data = data.sample(replace=True, frac=1)
     resampled_data["Cumulative_captures"] = data.Capturas.cumsum()

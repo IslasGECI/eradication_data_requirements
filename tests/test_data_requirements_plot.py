e5b2362f8bb12aeaab13c99e18014a961db484d6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-12 20:27:42 +0000

    🛑🧪 Fail tests

--- tests/test_data_requirements_plot.py
+++ tests/test_data_requirements_plot.py
@@ -64,7 +64,7 @@ def test_resample_eradication_data():
     data = pd.DataFrame({"CPUE": [19.5, 19, 18.5, 18, 17.5, 17], "Capturas": [1, 2, 3, 4, 5, 6]})
     sample = dt.resample_eradication_data(data)
     expected_columns_names = ["CPUE", "Cumulative_captures"]
-    assert sample.columns == expected_columns_names
+    assert (sample.columns == expected_columns_names).all()
 
 
 def test_fit_ramsey_plot():

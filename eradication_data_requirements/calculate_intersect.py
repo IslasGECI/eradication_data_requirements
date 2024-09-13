2c2543c9fd02c4b7c848da9c49624ac697620f98
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-13 17:52:50 +0000

    ✅ Pass tests

--- eradication_data_requirements/calculate_intersect.py
+++ eradication_data_requirements/calculate_intersect.py
@@ -24,7 +24,7 @@ def get_intercepts_distribution(raw_data, bootstrap_number, seed=None):
 
 def resample_eradication_data(data, rng):
     resampled_data = data.sample(replace=True, frac=1, random_state=rng)
-    resampled_data["Cumulative_captures"] = data.Capturas.cumsum()
+    resampled_data["Cumulative_captures"] = resampled_data.Capturas.cumsum()
     return resampled_data[["CPUE", "Cumulative_captures"]]
 
 

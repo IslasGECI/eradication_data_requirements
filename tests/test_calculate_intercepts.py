3d02767a0555e4bbff09aa14f8672c4a41583b4b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-13 20:49:23 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_intercepts.py
+++ tests/test_calculate_intercepts.py
@@ -8,12 +8,15 @@ raw_data = pd.DataFrame({"CPUE": [19.5, 19, 18.5, 18, 17.5, 17], "Capturas": [1,
 seed = 42
 
 
-def tests_get_intercept_latex_string():
+def tests_get_population_status_dict():
     bootstrap_number = 20
     obtained = edr.get_population_status_dict(raw_data, bootstrap_number, seed)
     assert isinstance(obtained, dict)
     expected_n0_interval = "188 (127 - 317)"
     assert obtained["n0"] == expected_n0_interval
+
+    expected_remanents = "157 (106 - 296)"
+    assert obtained["remanentes"] == expected_remanents
 
 
 def test_resample_eradication_data():

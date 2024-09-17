343fd9c55f136552f0a76813efc0ca63246b285f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-17 20:31:23 +0000

    🛑🧪 Fail tests

---
+++ tests/test_set_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from eradication_data_requirements import filter_data_by_method
+
+
+def test_filter_data_by_method():
+    raw_data = pd.DataFrame(
+        {
+            "Tecnica": ["Extracción viva (arreo)", "Cacería terrestre", "Cacería terrestre"],
+            "Capturas": [10, 20, 30],
+        }
+    )
+    method = "Cacería terrestre"
+    obtained = filter_data_by_method(raw_data, method)
+    expected_rows = 2
+    assert len(obtained) == expected_rows

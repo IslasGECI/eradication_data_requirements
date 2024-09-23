714f24eb229fb7c0f992ad49c5047b99a87dfcdf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-23 17:53:31 +0000

    ✅ Pass tests

--- eradication_data_requirements/resample_aerial_monitoring.py
+++ eradication_data_requirements/resample_aerial_monitoring.py
@@ -3,4 +3,7 @@ from bootstrapping_tools import resample_data
 
 def get_sum_distribution(df, bootstrap_number):
     blocks_length = 1
-    return [resample_data(df, seed, blocks_length) for seed in range(bootstrap_number)]
+    return [
+        int(resample_data(df, seed, blocks_length).No_goats.sum())
+        for seed in range(bootstrap_number)
+    ]

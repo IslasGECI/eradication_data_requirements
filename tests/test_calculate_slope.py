e131060508d6fe9a05f6a78aef8ab70d106a68e2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 16:43:41 +0000

    🛑🧪 Fail tests

--- tests/test_calculate_slope.py
+++ tests/test_calculate_slope.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from eradication_data_requirements.progress_probability import get_slope
 
@@ -7,4 +8,4 @@ def test_get_slope():
     data = pd.DataFrame({"CPUE": [2, 1], "Cumulative_captures": [1, 2]})
     obtained = get_slope(data)
     expected_slope = -1
-    assert obtained == expected_slope
+    assert pytest.approx(obtained) == expected_slope

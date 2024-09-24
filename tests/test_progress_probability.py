df601dc2acf28342f510f84d1c1ba066b066d51d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 16:47:35 +0000

    🛑🧪 Fail tests

--- tests/test_progress_probability.py
+++ tests/test_progress_probability.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import pytest
 
-from eradication_data_requirements.progress_probability import get_slope
+from eradication_data_requirements.progress_probability import get_slope, get_slopes_distribution
+
+
+def tests_get_slopes_distribution():
+    raw_data_2 = pd.DataFrame({"CPUE": [1, 1, 18.5, 18, 17.5, 27], "Capturas": [1, 2, 3, 4, 5, 6]})
+    bootstrap_number = 10
+    obtained = get_slopes_distribution(raw_data_2, bootstrap_number)
+    obtained_rows = len(obtained)
+    assert obtained_rows == bootstrap_number
 
 
 def test_get_slope():

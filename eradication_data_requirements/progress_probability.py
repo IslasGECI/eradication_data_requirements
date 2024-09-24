e360c7bef0e28f175cd7172e6b92f187fae3c4cb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-09-24 16:53:17 +0000

    ✅ Pass tests

--- eradication_data_requirements/progress_probability.py
+++ eradication_data_requirements/progress_probability.py
@@ -1,4 +1,12 @@
+import numpy as np
+
 from eradication_data_requirements.data_requirements_plot import fit_ramsey_plot
+from eradication_data_requirements.calculate_intersect import resample_eradication_data
+
+
+def get_slopes_distribution(raw_data, bootstrap_number):
+    rng = np.random.default_rng()
+    return [resample_eradication_data(raw_data, rng) for _ in range(bootstrap_number)]
 
 
 def get_slope(data):

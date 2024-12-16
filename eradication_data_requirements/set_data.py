a6cedd8d5d67f594781d5c6d3d8301987ae8f4da
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-16 18:26:39 +0000

    ✅ Pass tests

--- eradication_data_requirements/set_data.py
+++ eradication_data_requirements/set_data.py
@@ -2,3 +2,7 @@ def filter_data_by_method(raw_data, method):
     filtered_by_method = raw_data[raw_data.Tecnica == method]
     filtered_by_method.loc[:, ["Acumulado"]] = filtered_by_method.Capturas.cumsum()
     return filtered_by_method
+
+
+def select_december_of_every_year(data):
+    return data[data.Fecha.str.contains("-12-")]

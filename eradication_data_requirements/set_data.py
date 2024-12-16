18fe8eab3b82833c47069ddd8852608cda9e22a7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-12-16 23:02:35 +0000

    ✅ Pass tests

--- eradication_data_requirements/set_data.py
+++ eradication_data_requirements/set_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def filter_data_by_method(raw_data, method):
     filtered_by_method = raw_data[raw_data.Tecnica == method]
     filtered_by_method.loc[:, ["Acumulado"]] = filtered_by_method.Capturas.cumsum()
@@ -6,4 +9,5 @@ def filter_data_by_method(raw_data, method):
 
 def select_december_of_every_year(data):
     month_to_plot = "-12-"
-    return data[data.Fecha.str.contains(month_to_plot)]
+    cutted_months = data[data.Fecha.str.contains(month_to_plot)]
+    return pd.concat([cutted_months, data.iloc[-1:]])

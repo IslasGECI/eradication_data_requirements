69e8f383c9cf1ff5bdbb6f8846cceb2e6f941586
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-15 18:36:31 +0000

    🛑🧪 Fail tests

--- tests/test_mix_distributions.py
+++ tests/test_mix_distributions.py
@@ -3,10 +3,11 @@ import numpy as np
 
 
 def test_combine_distributions_from_dict():
-    distribution_a = np.random.normal(316, 5, 20)
+    rng = np.random.default_rng(seed=42)
+    distribution_a = rng.normal(316, 5, 20)
     print(distribution_a)
     pop_status_a = {"distribution": distribution_a, "capturas": 10}
-    distribution_b = np.random.normal(100, 10, 20)
+    distribution_b = rng.normal(100, 10, 20)
     print(distribution_b)
     pop_status_b = {"distribution": distribution_b, "capturas": 10}
     obtained = combine_distributions_from_dict(pop_status_a, pop_status_b)

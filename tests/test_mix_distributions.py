568649f4b90835c9f9e889bb67275ebae7d92fba
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-15 18:41:14 +0000

    🛑🧪 Fail tests

--- tests/test_mix_distributions.py
+++ tests/test_mix_distributions.py
@@ -6,9 +6,10 @@ def test_combine_distributions_from_dict():
     rng = np.random.default_rng(seed=42)
     distribution_a = rng.normal(316, 5, 20)
     print(distribution_a)
-    pop_status_a = {"distribution": distribution_a, "capturas": 10}
+    pop_status_a = {"distribution": distribution_a, "capturas": 20}
     distribution_b = rng.normal(100, 10, 20)
     print(distribution_b)
     pop_status_b = {"distribution": distribution_b, "capturas": 10}
     obtained = combine_distributions_from_dict(pop_status_a, pop_status_b)
     assert "remanentes" in obtained.keys()
+    assert obtained["capturas"] == 30

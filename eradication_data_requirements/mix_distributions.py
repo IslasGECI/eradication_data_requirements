3f11b8ddeef090d13353eeab4dddbcc59a73ebe6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-15 19:01:33 +0000

    ✅ Pass tests

--- eradication_data_requirements/mix_distributions.py
+++ eradication_data_requirements/mix_distributions.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+
 def combine_distributions_from_dict(pop_status_a, pop_status_b):
     capturas_totales = pop_status_a["capturas"] + pop_status_b["capturas"]
     return {"remanentes": "a (b — c)", "capturas": capturas_totales}
+
+
+def concatenate_remanent_distributions(pop_status_a, pop_status_b):
+    n0_a = pop_status_a["distribution"]
+    n0_b = pop_status_b["distribution"]
+    return np.concatenate((n0_a, n0_b))

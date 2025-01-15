b31b877107c037ef9f1cb00a41b99e56b3d920e7
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-01-15 18:57:59 +0000

    🛑🧪 Fail tests

--- tests/test_mix_distributions.py
+++ tests/test_mix_distributions.py
@@ -1,15 +1,26 @@
-from eradication_data_requirements.mix_distributions import combine_distributions_from_dict
+from eradication_data_requirements.mix_distributions import (
+    combine_distributions_from_dict,
+    concatenate_remanent_distributions,
+)
 import numpy as np
 
 
+rng = np.random.default_rng(seed=42)
+distribution_a = rng.normal(316, 5, 20)
+print(distribution_a)
+pop_status_a = {"distribution": distribution_a, "capturas": 20}
+distribution_b = rng.normal(100, 10, 20)
+print(distribution_b)
+pop_status_b = {"distribution": distribution_b, "capturas": 10}
+
+
 def test_combine_distributions_from_dict():
-    rng = np.random.default_rng(seed=42)
-    distribution_a = rng.normal(316, 5, 20)
-    print(distribution_a)
-    pop_status_a = {"distribution": distribution_a, "capturas": 20}
-    distribution_b = rng.normal(100, 10, 20)
-    print(distribution_b)
-    pop_status_b = {"distribution": distribution_b, "capturas": 10}
     obtained = combine_distributions_from_dict(pop_status_a, pop_status_b)
     assert "remanentes" in obtained.keys()
     assert obtained["capturas"] == 30
+
+
+def test_concatenate_remanent_distributions():
+    obtained = concatenate_remanent_distributions(pop_status_a, pop_status_b)
+    expected_len = len(distribution_b) + len(distribution_a)
+    assert len(obtained) == expected_len

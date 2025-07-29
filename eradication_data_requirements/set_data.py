30fae6dc65f3d668c6cdbeeda0390744b6e7f790
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-29 21:50:12 +0000

    ✅ Pass tests

--- eradication_data_requirements/set_data.py
+++ eradication_data_requirements/set_data.py
@@ -17,11 +17,9 @@ def select_december_of_every_year(data):
 def select_month_by_window_length(data, window_length):
 
     month_to_plot = [f"-{factor:02d}-" for factor in range(1, 13) if factor % window_length == 0]
-    print(month_to_plot)
     mask = reduce(
         lambda x, y: x | y,
         [data["Fecha"].str.contains(pattern, case=False) for pattern in month_to_plot],
     )
     cutted_months = data[mask]
-    print(cutted_months)
-    return pd.concat([cutted_months, data.iloc[-1:]])
+    return pd.concat([cutted_months, data.iloc[-1:]]).index

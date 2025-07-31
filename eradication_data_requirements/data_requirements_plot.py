cd915bec9002992261d473a40593fda676f0b1c8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-31 16:29:43 +0000

    ♻️  Refactor

--- eradication_data_requirements/data_requirements_plot.py
+++ eradication_data_requirements/data_requirements_plot.py
@@ -46,6 +46,10 @@ def plot_data_requirements_from_config_file(input_path, output_path, config_path
 
 def data_requirements_plot(input_path, output_path, config_plot):
     data = pd.read_csv(input_path)
+    return xxdata_requirements_plot(data, output_path, config_plot)
+
+
+def xxdata_requirements_plot(data, output_path, config_plot):
     configured_data = set_cumulative_captures_column(data, config_plot)
     _, ax = geci_plot()
     ax = plot_catch_curve(configured_data, ax)

695d63625a5af2f28b4fdeb244ecb2c17f6f0118
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-07-31 16:47:39 +0000

    ♻️  Refactor

--- eradication_data_requirements/data_requirements_plot.py
+++ eradication_data_requirements/data_requirements_plot.py
@@ -36,7 +36,7 @@ def plot_comparative_catch_curves(socorro_path, guadalupe_path, output_path):
 def plot_traps_data_requirements(input_path, output_path):
     config_plot = {"ylabel": "CPUE (captures/night traps)", "cumulative": "Cumulative_captures"}
     data = pd.read_csv(input_path)
-    ax = xxdata_requirements_plot(data, config_plot)
+    ax = data_requirements_plot(data, config_plot)
     plt.savefig(output_path, dpi=300, transparent=True)
     return ax
 
@@ -45,12 +45,12 @@ def plot_data_requirements_from_config_file(input_path, output_path, config_path
     with open(config_path, encoding="utf8") as config_file:
         config_plot = json.load(config_file)
     data = pd.read_csv(input_path)
-    ax = xxdata_requirements_plot(data, config_plot)
+    ax = data_requirements_plot(data, config_plot)
     plt.savefig(output_path, dpi=300, transparent=True)
     return ax
 
 
-def xxdata_requirements_plot(data, config_plot):
+def data_requirements_plot(data, config_plot):
     configured_data = set_cumulative_captures_column(data, config_plot)
     _, ax = geci_plot()
     ax = plot_catch_curve(configured_data, ax)

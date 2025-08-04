90789ff3f1bc82a4c150e4adf21e87a8c59991c3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-08-04 17:54:09 +0000

    ♻️  Refactor

--- eradication_data_requirements/api.py
+++ eradication_data_requirements/api.py
@@ -4,7 +4,7 @@ from eradication_data_requirements.calculate_intersect import get_population_sta
 from eradication_data_requirements.cli import plot_cumulative_series_cpue_by_flight
 from eradication_data_requirements.data_requirements_plot import (
     plot_traps_data_requirements,
-    plot_comparative_catch_curves,
+    xxplot_comparative_catch_curves,
     data_requirements_plot,
 )
 from eradication_data_requirements.mix_distributions import combine_distributions_from_dict
@@ -148,4 +148,7 @@ async def api_plot_cumulative_series_cpue_by_flight(input_path: str, output_path
 async def api_plot_comparative_catch_curves(
     socorro_path: str, guadalupe_path: str, output_path: str
 ):
-    plot_comparative_catch_curves(socorro_path, guadalupe_path, output_path)
+    socorro_data = pd.read_csv(socorro_path)
+    guadalupe_data = pd.read_csv(guadalupe_path)
+    xxplot_comparative_catch_curves(socorro_data, guadalupe_data)
+    plt.savefig(output_path, dpi=300, transparent=True)

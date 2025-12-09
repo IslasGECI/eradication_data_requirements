fec5273a724332e4da93e39362a3af36eb9e832e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-12-09 18:57:01 +0000

    🛑🧪 Fail tests

--- tests/test_set_data.py
+++ tests/test_set_data.py
@@ -62,6 +62,11 @@ def tests_select_month_by_window_length():
         }
     )
     window_length = 12
+    obtained = select_month_by_window_length(data.iloc[0:12], window_length)
+    expected_number_of_rows = 1
+    obtained_number_of_rows = len(obtained)
+
+    assert obtained_number_of_rows == expected_number_of_rows
     obtained = select_month_by_window_length(data, window_length)
     expected_number_of_rows = 2
     obtained_number_of_rows = len(obtained)
